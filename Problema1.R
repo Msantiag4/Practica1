623f6f1a05be816d9ddf9b1c5f83da513f15b5c8
Msantiag4 <159881821+Msantiag4@users.noreply.github.com>
2024-02-20 09:33:02 +0100

    Update Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,14 @@
 #Codigo para problema 1
 
 plot(1:5)
+mis_dades=mtcars
+dim(mis_dades)
+
+mean(mis_dades$qsec)
+median(mis_dades$drat)
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$mpg, 0.18)
+IQR(mis_dades$cyl)
+
+sd(mis_dades$cyl)
+var(mis_dades$qsec)
